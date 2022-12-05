6812cadc518d2f8be236d2ef5585f8b961fe23fa
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-12-05 12:55:02 +0100

    correction challenge 7

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-//La note était de 275, le pourboire de 41,25 et la valeur totale était de 316,25
+const calcTipPercent = (bill) => {
+  if (bill > 100 && bill < 200) return 10;
+  if (bill > 50 && bill < 300) return 15;
+  return 20;
+}
 
-const bill = 275;
+const bills = [125, 555, 44];
 
+/* let tips = [];
+
+for (let i = 0; i < bills.length; i++) {
+  tips[i] = bills[i] * calcTipPercent(bills[i]) / 100;
+}
+
+bills.forEach((bill) => {
+  tips.push(bill * calcTipPercent(bill) / 100);
+}); */
+
+const tips = bills.map((bill) => {
+  return bill * calcTipPercent(bill) / 100;
+});
+
+let totals = [];
+for (let i = 0; i < bills.length; i++) {
+  totals.push(bills[i] + tips[i]);
+}
+
+
+
+
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
+
+
+/* const bill = 275;
 let percent = 15;
 
 if (bill < 50 || bill > 300) {
@@ -12,3 +45,4 @@ const tip = bill * percent / 100;
 const total  = bill + tip;
 
 console.log(`La note était de ${bill}, le pourboire de ${tip} et la valeur totale était de ${total}`);
+ */
